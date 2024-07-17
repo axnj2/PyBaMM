a792f8a63b4848de76802a44cd81f55e67c4d0e7
axnj2 <52408308+axnj2@users.noreply.github.com>
2024-07-17 14:17:25 +0200

    full SOC range

--- EISdifferentSOC.py
+++ EISdifferentSOC.py
@@ -1,5 +1,6 @@
 # in this code u can generate EIS plot for different SOCs; you can also choose the ambient temperature
 # original code by Ali Rahdarian
+import itertools
 import os
 
 import pybamm
@@ -23,22 +24,12 @@ I_max = 10 * 1e-3
 C = 50  # temperature in degrees Celsius
 T_amb = 273.15 + C  # converting to Kelvin
 
-# Set the SOC
-soc = 0.5  # np.linspace(0.05, 0.95, 19)  # todo parametrize this
+number_of_periods = 15
+samples_per_period = 5
 
 
-# number_of_periods = 15
-# samples_per_period = 5
-
-
-def init(input_number_of_periods, input_numper_of_samples_per_period):
+def init():
     global sim
-    global init_time
-    global number_of_periods
-    global samples_per_period
-    number_of_periods = input_number_of_periods
-    samples_per_period = input_numper_of_samples_per_period
-    start = time.time()
     # Set up the model and parameters
     model = pybamm.lithium_ion.DFN(options={"surface form": "differential"}, name="DFN")
     parameter_values = pybamm.ParameterValues("Prada2013")
@@ -50,11 +41,13 @@ def init(input_number_of_periods, input_numper_of_samples_per_period):
     sim = pybamm.Simulation(
         model, parameter_values=parameter_values, solver=pybamm.CasadiSolver(atol=1e-9, rtol=1e-9, mode="fast")
     )
-    end = time.time()
-    init_time = end - start
 
 
-def simulate_frequency(frequency):
+def return_args(soc, frequency):
+    return soc, frequency
+
+
+def simulate_frequency(soc, frequency):
     start = time.time()
 
     # Solve
@@ -73,61 +66,59 @@ def simulate_frequency(frequency):
     idx = np.argmax(np.abs(current_fft))
     impedance = -voltage_fft[idx] / current_fft[idx]
 
-    end = time.time()
-    return impedance, end - start, init_time
+    return impedance
 
 
 if __name__ == "__main__":
-    frequencies = np.logspace(-2, 3, 20)
-    N_frequencies = len(frequencies)
+    # Set the SOC
+    number_of_SOC = 19
+    SOC_RANGE = np.linspace(0.05, 0.95, number_of_SOC)
+
+    N_frequencies = 30
+    frequencies = np.logspace(-2, 3, N_frequencies)
 
     # List of SOC values
     soc_values = [0.5]  # np.arange(0.05, 1.00, 0.05)
     N_socs = len(soc_values)  # length of frequency vector
-    impedences_time = []
 
-    for k, (number_of_periods, samples_per_period) in enumerate([(15, 5), (150, 50)]):
-        print(k, number_of_periods, samples_per_period)
-        impedances_soc = {}  # Initialize a dictionary to store impedance data for each SOC
-        impedance_data = np.zeros((N_socs, N_frequencies), dtype=complex)  # data to be stored for the prject
-        jj = 0
+    impedances_soc = {}  # Initialize a dictionary to store impedance data for each SOC
+    impedance_data = np.zeros((N_socs, N_frequencies), dtype=complex)  # data to be stored for the project
+    jj = 0
 
-        # Loop over SOC values
-        for soc in tqdm(soc_values):
-            with Pool(os.cpu_count(), initializer=init,
-                      initargs=(number_of_periods, samples_per_period)) as p:
-                pool_output = list(p.imap(simulate_frequency, frequencies))
+    # Loop over SOC values
+    for i in tqdm(range(1)):
+        with Pool(os.cpu_count(), initializer=init) as p:
+            pool_output = list(p.starmap(simulate_frequency, itertools.product(SOC_RANGE, frequencies)))
 
-            process_execution_time = [i[1] for i in pool_output]
-            impedences_time.append([i[0] for i in pool_output])
-            init_times = [i[2] for i in pool_output]
+    for k in range(number_of_SOC):
+        impedances_soc[float(SOC_RANGE[k])] = pool_output[k * N_frequencies: (k + 1) * N_frequencies]
 
-            # Store the impedance data for this SOC
-            impedances_soc[soc] = 1000 * np.array(impedences_time[k])
-            impedance_data[jj] = 1000 * np.array(impedences_time[k])
-            jj = jj + 1
+    pprint(impedances_soc)
 
-        # pprint(process_execution_time)
-        # pprint(init_times)
-        # # Specify the file name
-        # filename = 'impedance_data_50degrees.mat'
-        # # Save the complex data to a .mat file
-        # savemat(filename, {'impedance_data_50degrees': impedance_data})
-        # Plot all impedances with their corresponding labels
-        plt.figure()
-        cmap = plt.get_cmap('tab20')
-        for i, (soc, data) in enumerate(impedances_soc.items()):
-            plt.plot(data.real, -data.imag, marker='o', label=f'SOC = {soc}', color=cmap(i % 20))
-        plt.xlabel(r"$Z_\mathrm{Re}$ [mOhm]")
-        plt.ylabel(r"$-Z_\mathrm{Im}$ [mOhm]")
-        plt.legend()
-        plt.show()
+    # process_execution_time = [i[1] for i in pool_output]
+    # impedences_time = [i[0] for i in pool_output]
+    # init_times = [i[2] for i in pool_output]
 
+    # Store the impedance data for this SOC
+    # impedances_soc[soc] = 1000 * np.array(impedences_time)
+    # impedance_data[jj] = 1000 * np.array(impedences_time)
+    # jj = jj + 1
+
+    # pprint(process_execution_time)
+    # pprint(init_times)
+    # # Specify the file name
+    # filename = 'impedance_data_50degrees.mat'
+    # # Save the complex data to a .mat file
+    # savemat(filename, {'impedance_data_50degrees': impedance_data})
+    # Plot all impedances with their corresponding labels
     plt.figure()
-    plt.plot(frequencies, [np.abs(approx - ground_truth) / np.abs(ground_truth) for approx, ground_truth in
-                           zip(impedences_time[0], impedences_time[1])],
-             label='error')
-    plt.xscale('log')
+    cmap = plt.get_cmap('tab20')
+    for i, (soc, data) in enumerate(impedances_soc.items()):
+        plt.plot(data.real, -data.imag, marker='o', label=f'SOC = {soc}', color=cmap(i % 20))
+    plt.xlabel(r"$Z_\mathrm{Re}$ [mOhm]")
+    plt.ylabel(r"$-Z_\mathrm{Im}$ [mOhm]")
+    plt.legend()
     plt.show()
+
     # Optional: Plot the last simulation result variables
     #    sim.plot(output_variables=["Current [A]", "Terminal voltage [V]"])
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+# test that were made for multiprocessing
+
+for 1000 frequencies (15 periods and 5 samples per periods, 1e-9 precision) :
+
+| method    | time(s) |
+|-----------|---------|
+| map_async | 12.63   |
+| map       | 12.85   |
+| imap      | 12.85   |
+
+so pretty much the same time for all the methods
+
+## how to launch the code on windows
+
+```bash
 C:\Users\lab07> cd C:\Users\lab07\Documents\pybamm2
 
 C:\Users\lab07\Documents\pybamm2>python -m venv ./venv/
@@ -7,9 +22,12 @@ C:\Users\lab07\Documents\pybamm2>.\venv\Scripts\activate.bat
 (venv) C:\Users\lab07\Documents\pybamm2>pip install pybamm tqdm matplotlib
 
 (venv) C:\Users\lab07\Documents\pybamm2>python EISdifferentSOC.py
-
+```
 
 and the next time :
+
+```bash
 C:\Users\lab07> cd C:\Users\lab07\Documents\pybamm2
 C:\Users\lab07\Documents\pybamm2>.\venv\Scripts\activate.bat
 (venv) C:\Users\lab07\Documents\pybamm2>python EISdifferentSOC.py
+```
